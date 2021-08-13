1f23fd810bf1ef7bbac1848f51d10ca95bb8b030
Akshay <adominic21@gmail.com>
2021-08-14 02:44:58 +0530

    added 2 functions

---
+++ 1.Problems/CharCount.js
@@ -0,0 +1,14 @@
+function charCount(str){
+    let obj = {};
+    for(let char of str){
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)){
+            if(obj[char]>0){
+                obj[char]++;
+            }else{
+                obj[char]=1;
+            }
+        }
+    }
+    return obj;
+}
